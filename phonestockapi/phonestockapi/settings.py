63662b5c4a1a233a890bc909c1819230ffafd2be
willy <rutayisirewilly9@gmail.com>
2020-09-17 11:11:16 +0200

    ui

--- phonestockapi/phonestockapi/settings.py
+++ phonestockapi/phonestockapi/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ('SECRET_KEY')
+SECRET_KEY = '-8-z52tasn5f)5i+x8=qtr0^$%@$ke0wyhfz)wf@xms0v@x32o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
